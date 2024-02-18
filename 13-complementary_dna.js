52a45edcb790d6fa341441005852999eec1ea5a9
gozalie <fikri.gozali06@gmail.com>
2024-02-18 09:01:25 +0700

    day 21

---
+++ 13-complementary_dna.js
@@ -0,0 +1,55 @@
+/*
+Problem:
+
+Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+Example: (input --> output)
+
+"ATTGC" --> "TAACG"
+"GTAT" --> "CATA"
+*/
+
+/* answer */
+
+/*
+A = T
+T = A
+C = G
+G = C
+
+*/
+
+// function dnaStrand(dna) {
+//   let result = "";
+//   for (let i = 0; i < dna.length; i++) {
+//     if (dna[i] === "A") {
+//       result += "T";
+//     } else if (dna[i] === "T") {
+//       result += "A";
+//     } else if (dna[i] === "C") {
+//       result += "G";
+//     } else if (dna[i] === "G") {
+//       result += "C";
+//     }
+//   }
+//   return result;
+// }
+
+// oneliner
+// const dnaStrand = (dna) => [...dna].map((str) => (str === "A" ? "T" : str === "T" ? "A" : str === "G" ? "C" : "G")).join("");
+
+// or
+
+function dnaStrand(dna) {
+  const pairs = { A: `T`, T: `A`, C: `G`, G: `C` };
+  return [...dna].map((str) => pairs[str]).join("");
+}
+
+console.log(dnaStrand("ATTGC"));
+console.log(dnaStrand("TTTAAAAACCCCGGGG"));
---
+++ 14-credit_card_mask.js
@@ -0,0 +1,32 @@
+/*
+Problem:
+
+Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+Examples (input --> output)
+"4556364607935616" --> "############5616"
+"64607935616" -->      "#######5616"
+"1" -->                "1"
+"" -->                 ""
+
+// "What was the name of your first pet?"
+"Skippy" --> "##ippy"
+"Nananananananananananananananana Batman!" --> "####################################man!"
+*/
+
+/* answer */
+// const maskify = (cc) => [...cc].reverse().map((str, i) => i >= 4 ? str = "#" : str).reverse().join("");
+
+// or
+
+const maskify = (cc) => cc.slice(-4).padStart(cc.length, "#");
+
+// or
+
+// const maskify = (cc) => "#".repeat(cc.length - 4) + cc.slice(-4);
+
+console.log(maskify("4556364607935616"));
+console.log(maskify("11111"));
+console.log(maskify("Nananananananananananananananana Batman!"));
