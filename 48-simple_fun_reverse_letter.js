c3b75ff000bc82fd0b47bb9c171640c771d1239c
gozalie <fikri.gozali06@gmail.com>
2024-03-13 00:55:47 +0700

    day 46

---
+++ 48-simple_fun_reverse_letter.js
@@ -0,0 +1,27 @@
+/*
+Problem:
+
+Task
+Given a string str, reverse it and omit all non-alphabetic characters.
+
+Example
+For str = "krishan", the output should be "nahsirk".
+
+For str = "ultr53o?n", the output should be "nortlu".
+
+Input/Output
+[input] string str
+A string consists of lowercase latin letters, digits and symbols.
+
+[output] a string
+
+*/
+
+/* answer */
+const reverseLetter = (str) =>
+  [...str]
+    .filter((string) => string >= "a" && string <= "z")
+    .reverse()
+    .join("");
+
+console.log(reverseLetter("ultr53o?n"));
